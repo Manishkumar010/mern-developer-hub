c819a6e3f3f75c71e9823464f3ea76ded30e6598
Manishkumar010 <mk519956@gmail.com>
2025-08-26 18:51:38 +0530

    google analyst added 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${GeistSans.className}`}>
         {children}
         <GoogleAnalytics gaId="G-1KRDR061H3" />
+        <Analytics/>
       </body>
     </html>
   );
