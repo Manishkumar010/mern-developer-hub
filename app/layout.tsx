03dab03c6005fb35757a91ef534a4dffe6e2b6a2
Manishkumar010 <mk519956@gmail.com>
2025-08-26 18:37:29 +0530

    google analyst added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className={`${GeistSans.className}`}>
         {children}
+        <GoogleAnalytics gaId="G-1KRDR061H3" />
       </body>
     </html>
   );
